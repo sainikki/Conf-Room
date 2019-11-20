002d3a1464c21fa1e925459ff70a4e1916c50e45
hitanashim <53200502+hitanashim@users.noreply.github.com>
2019-11-19 20:55:09 -0500

    Update kiosk.component.ts

--- src/app/kiosk/kiosk.component.ts
+++ src/app/kiosk/kiosk.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DetailsService } from './schedule/DetailsService';  
+
 @Component({
   selector: 'app-kiosk',
   templateUrl: './kiosk.component.html',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KioskComponent implements OnInit {
 
-  constructor() { }
+  
+  location:  string;
+  summary:  string;
+  start_time:  String;
+  end_time: String;
+  start_date:  String;
 
+ selectedRoom: Location;
+ roomSelected: any[];
+
+  public Details;
+
+  //initialize the call using StudentService 
+  constructor(private _myService: DetailsService) { }
   ngOnInit() {
+    this.getDetails();
+  }
+  // subtitleFormat callback example
+  onSubmit()  {
+    // console.log(this.bookingModel);
+    this.selectedRoom=location;
   }
 
+  calculate()
+  {
+
+  }
+
+  starter()
+  {
+    
+  }
+  //method called OnInit
+  getDetails() {
+   this._myService.getDetails().subscribe(
+      //read data and assign to public variable students
+      data => { this.Details = data},
+      err => console.error(err),
+      () => console.log('finished loading')
+    );
+  }
 }
+
+
+
+
