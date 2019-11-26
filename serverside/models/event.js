3838120e6e49acb05b309935e0925b063d043833
sh-shmss <sh.shmss@gmail.com>
2019-11-25 20:29:08 -0500

    backend added

--- serverside/models/event.js
+++ serverside/models/event.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-//define a schema/ blueprint NOTE: id is not a part of the schema 
+//define a schema/ blueprint NOTE: id is not a part of the schema
   const eventSchema = new mongoose.Schema({
    // id:{type:int,required:true},
   //  location:  { type: String, required: true},
@@ -8,7 +8,6 @@ const mongoose = require('mongoose');
    end_time: { type: String, required: false},
    startdate: {type: Date,required:false},
    enddate: {type: Date,required:false},
-  
    summary:  { type: String, required: true},
    start_time:  { type: String, required: false},
 
@@ -16,8 +15,8 @@ const mongoose = require('mongoose');
 
 
 
-//use the blueprint to create the model 
+//use the blueprint to create the model
 // Parameters: (model_name, schema_to_use, collection_name)
-//module.exports is used to allow external access to the model  
+//module.exports is used to allow external access to the model
 module.exports = mongoose.model('Event', eventSchema,'event');
 //note capital S in the collection name
